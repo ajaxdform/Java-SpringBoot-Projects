48eab1b95e5153d4ae978bbe27a84bdbdb85762c
Shubham Salaskar <shubhamsalaskar03@gmail.com>
2025-01-09 15:47:44 +0530

    Antim commit

---
+++ Rest API Spring Boot/src/test/java/com/jdbc_template/intracting_with_data/TestDataUtil.java
@@ -0,0 +1,83 @@
+package com.jdbc_template.intracting_with_data;
+
+import com.jdbc_template.intracting_with_data.domain.dto.AuthorDto;
+import com.jdbc_template.intracting_with_data.domain.dto.BookDto;
+import com.jdbc_template.intracting_with_data.domain.entities.AuthorEntity;
+import com.jdbc_template.intracting_with_data.domain.entities.BookEntity;
+
+public final class TestDataUtil {
+    private TestDataUtil() {
+
+    }
+
+    public static AuthorEntity createTesAuthorA() {
+        return AuthorEntity.builder()
+            .id(1L)
+            .name("Shubham Salaskar")
+            .age(80)
+            .build();
+    }
+
+    public static AuthorDto testAuthorDtoA() {
+        return AuthorDto.builder()
+            .id(1L)
+            .name("Shubham Salaskar")
+            .age(80)
+            .build();
+    }
+
+    public static AuthorEntity createTesAuthorB() {
+        return AuthorEntity.builder()
+            .id(2L)
+            .name("Vaibhav Shinde")
+            .age(44)
+            .build();
+    }
+
+    public static AuthorEntity createTesAuthorC() {
+        return AuthorEntity.builder()
+            .id(3L)
+            .name("Harshita XX")
+            .age(24)
+            .build();
+    }
+
+    public static BookEntity createTestbookEntityA(final AuthorEntity author) {
+        return BookEntity.builder()
+        .isbn("3445-332-5566-4433")
+        .title("Negotitor")
+        .author(author)
+        .build();
+    }
+    public static BookDto createTestbookDtoA(final AuthorDto author) {
+        return BookDto.builder()
+        .isbn("3445-332-5566-4433")
+        .title("Negotitor")
+        .author(author)
+        .build();
+    }
+
+    public static BookDto createTestBookA(final AuthorDto author) {
+        return BookDto.builder()
+        .isbn("3445-332-5566-4433")
+        .title("Negotitor")
+        .author(author)
+        .build();
+    }
+
+    public static BookEntity createTestbookB(final AuthorEntity author) {
+        return BookEntity.builder()
+        .isbn("3445-332-5566-7736")
+        .title("The Scars")
+        .author(author)
+        .build();
+    }
+
+    public static BookEntity createTestbookC(final AuthorEntity author) {
+        return BookEntity.builder()
+        .isbn("3445-332-5566-7765")
+        .title("The Life")
+        .author(author)
+        .build();
+    }
+}
